fda8cfacc4313bba7354aca6fc32b6be7ea8723b
root <root@localhost.localdomain>
2022-05-26 05:00:01 -0400

    license url changes

--- tvk-oneclick/install_license.py
+++ tvk-oneclick/install_license.py
@@ -1,18 +1,19 @@
+#!/usr/bin/python3
 from bs4 import BeautifulSoup
 import requests
 import sys
 import subprocess
-
+import os
+ns = os.getenv('NAMESPACE')
 headers = {'Content-type': 'application/x-www-form-urlencoded; charset=utf-8'}
-endpoint="https://doc.trilio.io:5000/8d92edd6-514d-4acd-90f6-694cb8d83336/0061K00000fwkzU"
-result = subprocess.check_output("kubectl get ns kube-system -o=jsonpath='{.metadata.uid}'", shell=True)
+endpoint="https://license.trilio.io/8d92edd6-514d-4acd-90f6-694cb8d83336/0061K00000i9ORf"
+command = "kubectl get namespace "+ns+" -o=jsonpath='{.metadata.uid}'"
+result = subprocess.check_output(command, shell=True)
 kubeid = result.decode("utf-8")
 data = "kubescope=clusterscoped&kubeuid={0}".format(kubeid)
 r = requests.post(endpoint, data=data, headers=headers)
 contents=r.content
 soup = BeautifulSoup(contents, 'lxml')
-sys.stdout = open("license_file1.yaml", "w")
-print(soup.body.find('div', attrs={'class':'yaml-content'}).text)
-sys.stdout.close()
-result = subprocess.check_output("kubectl apply -f license_file1.yaml", shell=True)
-
+apply_command = soup.body.find('div', attrs={'class':'yaml-content'}).text
+print("creating license for "+ns)
+result = subprocess.check_output(apply_command.replace('kubectl', 'kubectl -n '+ns), shell=True)
